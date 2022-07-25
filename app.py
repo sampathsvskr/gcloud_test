7888a5aa3f07c4537828e3388b48e204d0519b7b
svskr <sampathkumarreddysvskr@gmail.com>
2022-07-25 19:33:05 +0530

    testing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello():	
-	return "Hoooo! Welcome to Dockerized app........."
+	return "Hoooo! Welcome to Dockerized app version 2"
 
 
 if __name__=="__main__":
