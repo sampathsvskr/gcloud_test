5f53c4b6ddf2c244d5015dbbf70b61e6c52f5ca6
svskr <sampathkumarreddysvskr@gmail.com>
2022-07-25 18:36:20 +0530

    updated_commit

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from redis import Redis
 
 app=Flask(__name__)
-redis=Redis(host='redis',port=6379)
+
 
 @app.route('/')
-def hello():
-	redis.incr('hits')
-	counter=str(redis.get('hits'),'utf-8')
-	return "hits count: "+counter
+def hello():	
+	return "Welcome to Dockerized app"
 
 
 if __name__=="__main__":
