58802b2fb1b6ffaf6d7ae2722b1f6633fd0740a4
svskr <sampathkumarreddysvskr@gmail.com>
2022-07-25 18:54:19 +0530

    upadted app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app=Flask(__name__)
 
@@ -9,4 +10,4 @@ def hello():
 
 
 if __name__=="__main__":
-	app.run(host="0.0.0.0",debug=True)
+	app.run(host="0.0.0.0",debug=True, port=int(os.environ.get("PORT", 8080)))
